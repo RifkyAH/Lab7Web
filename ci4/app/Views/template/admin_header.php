5bb3ffd240f97e84b0bf77665879592a76bc785f
RifkyAH <rifkyabdillah223@gmail.com>
2023-05-22 14:03:42 +0700

    update

---
+++ ci4/app/Views/template/admin_header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title><?= $title; ?></title>
+    <link rel="stylesheet" href="<?= base_url('/style.css'); ?>">
+</head>
+
+<body>
+    <div id="container">
+        <header>
+            <h1>Halaman Admin</h1>
+        </header>
+        <nav>
+            <a href="<?= base_url('/admin/artikel'); ?>" class="active">Home</a>
+            <a href="<?= base_url('/artikel'); ?>">Artikel</a>
+            <a href="<?= base_url('/admin/artikel/add'); ?>">Tambah Artikel</a>
+            <a href="<?= base_url('/admin/logout'); ?>">Logout</a>
+        </nav>
+        <section id="wrapper">
+            <section id="main">
